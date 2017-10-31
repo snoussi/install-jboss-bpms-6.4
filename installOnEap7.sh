eb2b6a4a40c83f79cff172868cca5a1c69269fd5
Rachid Snoussi <rsnoussi@redhat.com>
2017-10-31 16:14:56 +0100

    first commit

---
+++ installOnEap7.sh
@@ -0,0 +1,161 @@
+#!/bin/sh
+
+PRODUCT="Red Hat JBoss BPM Suite 6.4 on EAP7"
+
+BPMS=jboss-bpmsuite-6.4.0.GA-deployable-eap7.x
+BPMS_PATCH_WILDCARD=jboss-bpmsuite-6.4.?-patch*
+BPMS_PATCH=jboss-bpmsuite-6.4.5-patch
+
+EAP=jboss-eap-7.0.0
+EAP_PATCH=jboss-eap-7.0.8-patch
+
+EAP_USER=admin
+EAP_PWD=jboss@eap1
+
+BPMS_USER=bpmsAdmin
+BPMS_PWD=jboss@bpms1
+
+TARGET=../
+SRC_DIR=./installs
+
+JBOSS_HOME=$TARGET/jboss-eap-7.0
+BPMS_HOME=$TARGET/jboss-bpms-6.4
+
+echo
+echo "##############################################################"
+echo "##                                                          ##"
+echo "      Installing ${PRODUCT}"
+echo "##                                                          ##"
+echo "##                                                          ##"
+echo "##############################################################"
+echo
+
+# make some checks first before proceeding.
+if [ -r $SRC_DIR/$EAP.zip ] || [ -L $SRC_DIR/$EAP.zip ]; then
+  echo "JBoss Product sources $EAP are present..."
+  echo
+else
+  echo "Need to download $EAP.zip package from the Customer Portal"
+  echo "and place it in the $SRC_DIR directory to proceed..."
+  echo
+  exit
+fi
+
+if [ -r $SRC_DIR/$EAP_PATCH.zip ] || [ -L $SRC_DIR/$EAP_PATCH.zip ]; then
+  echo "JBoss Product patches $EAP_PATCH.zip are present..."
+  echo
+else
+  echo "Need to download $EAP_PATCH.zip package from the Customer Portal"
+  echo "and place it in the $SRC_DIR directory to proceed..."
+  echo
+  exit
+fi
+
+
+if [ -r $SRC_DIR/$BPMS.zip ] || [ -L $SRC_DIR/$BPMS.zip ]; then
+  echo "JBoss Product sources $BPMS.zip are present..."
+  echo
+else
+  echo "Need to download $BPMS.zip package from the Customer Portal"
+  echo "and place it in the $SRC_DIR directory to proceed..."
+  exit
+fi
+
+echo
+echo "Need to download all patch package from the Customer Portal" 
+echo "and place it in the $SRC_DIR directory to proceed..."
+for f in $BPMS_PATCH_WILDCARD; do
+  if [ -r $SRC_DIR/$f ] || [ -L $SRC_DIR/$f ]; then
+    echo "JBoss Product patches $f are present..."
+  fi
+done
+
+
+# Remove the old JBoss instance, if it exists.
+if [ -x $JBOSS_HOME ]; then
+  echo "  - existing JBoss product install detected in $JBOSS_HOME !"
+  echo "  - existing JBoss product install removed..."
+  echo
+  rm -rf $TARGET
+fi
+if [ -x $SRC_DIR/patch_tmp ]; then
+  echo "  - existing JBoss product patch_tmp detected and removed..."
+  echo
+  rm -rf ./patch_tmp
+fi
+
+# Install EAP
+echo
+echo "JBoss EAP installer running now..."
+echo
+unzip -qo $SRC_DIR/$EAP.zip -d $TARGET
+if [ $? -ne 0 ]; then
+  echo
+  echo "Error occurred during JBoss EAP installation!"
+  exit
+fi
+
+# Add a default admin user.
+echo
+echo "Adding a default admin user on JBoss EAP ..."
+echo
+$JBOSS_HOME/bin/add-user.sh $EAP_USER $EAP_PWD --silent
+if [ $? -ne 0 ]; then
+  echo
+  echo "Error occurred during JBoss EAP installation!"
+  exit
+fi
+
+echo
+echo "Applying $EAP_PATCH.zip patch now..."
+echo
+$JBOSS_HOME/bin/jboss-cli.sh --command="patch apply $SRC_DIR/$EAP_PATCH.zip --override-all"
+
+if [ $? -ne 0 ]; then
+ echo
+ echo "Error occurred during JBoss EAP patching!"
+ exit
+fi
+
+echo
+echo "Deploying $PRODUCT now..."
+echo
+unzip -qo $SRC_DIR/$BPMS.zip -d $TARGET
+
+if [ $? -ne 0 ]; then
+  echo
+  echo "Error occurred during $PRODUCT installation!"
+  exit
+fi
+
+echo
+echo "Applying patches on $PRODUCT now..."
+echo
+mkdir -p $SRC_DIR/patch_tmp
+for f in $BPMS_PATCH_WILDCARD; do
+  unzip -qo $SRC_DIR/$f -d $SRC_DIR/patch_tmp
+  (cd $SRC_DIR/patch_tmp/${f::-4} && exec ./apply-updates.sh ../../$JBOSS_HOME eap7.x);
+done
+
+rm -rf ./patch_tmp
+if [ $? -ne 0 ]; then
+  echo
+  echo "Error occurred during $PRODUCT installation!"
+  exit
+fi
+
+# Create initial users
+echo
+echo "Creating initial users ..."
+echo
+$JBOSS_HOME/bin/add-user.sh -a -r ApplicationRealm -u $BPMS_USER -p $BPMS_PWD -ro admin,analyst,user,kie-server,rest-all,agent,Administrators --silent
+if [ $? -ne 0 ]; then
+  echo
+  echo "Error occurred during JBoss EAP installation!"
+  exit
+fi
+
+echo
+echo "Renaming folders ..."
+echo
+mv $JBOSS_HOME $BPMS_HOME
---
+++ installs/README.md
@@ -0,0 +1,26 @@
+# Installation packages
+
+Below the list of installation packages to download from the Red Hat Customer Portal:
+
+### EAP 7.0 packages
+- Red Hat JBoss Enterprise Application Platform 7.0.0
+- Red Hat JBoss Enterprise Application Platform 7.0 Update 08
+
+### BPMS 6.4 packages [1]
+- Red Hat JBoss BPM Suite 6.4.0 Deployable for EAP 7
+- Red Hat JBoss BPM Suite 6.4 Update 1
+- Red Hat JBoss BPM Suite 6.4 Update 2
+- Red Hat JBoss BPM Suite 6.4 Update 3
+- Red Hat JBoss BPM Suite 6.4 Update 4
+- Red Hat JBoss BPM Suite 6.4 Update 5
+- Red Hat JBoss BPM Suite 6.4 Update 6 [2]
+
+### BPMS 6.4 offline maven repository
+- Red Hat JBoss BPM Suite 6.4.0 Maven Repository
+- Red Hat JBoss BPM Suite 6.4 Update 6 Incremental Maven Repository
+
+### BPMS 6.4 supplementary tools (including database scripts)
+- Red Hat JBoss BPM Suite 6.4.0 Supplementary Tools
+
+[1] Please note that, JBoss BPMS patches are not cumulative, all the update packages need to be applied to the main installation.
+[2] This patch introduces a small change into the database schema. You must apply the bpms-6.4-to-7.0.sql script to your database before you run Red Hat JBoss BPM Suite or Red Hat JBoss BRMS 6.4.6. Detailed instructions on how to obtain this script can be found in the following Red Hat Knowledgebase article : https://access.redhat.com/solutions/3211361
